2344342cf901bfe254cc3460c075786c997a78e4
Wei Zhang <wei.zhang@tacc.utexas.edu>
2025-09-10 16:03:48 -0500

    update the fix

--- tapis-jobsapi/src/main/java/edu/utexas/tacc/tapis/jobs/api/model/SubmitContext.java
+++ tapis-jobsapi/src/main/java/edu/utexas/tacc/tapis/jobs/api/model/SubmitContext.java
@@ -824,7 +824,7 @@ public final class SubmitContext
         // If exec system not specified in job request or app it is an error.
         if (StringUtils.isBlank(execSystemId))
         {
-            String msg = MsgUtils.getMsg("TAPIS_JOBS_MISSING_SYSTEM", "execution");
+            String msg = JobUtils.getMsg("JOBS_NO_EXEC_SYSTEM_SPECIFIED", null);
             throw new TapisImplException(msg, Status.BAD_REQUEST.getStatusCode());
         }
         

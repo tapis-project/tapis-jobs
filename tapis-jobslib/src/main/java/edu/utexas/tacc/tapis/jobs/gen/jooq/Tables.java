7623966a76e65afcf2abf54c0a6ff0a5988cf71a
Steve Black <51328106+scblack321@users.noreply.github.com>
2025-09-11 11:00:00 -0500

    Jobs: Switch from use of tapis-jobsmigrate module to DB init scripts similar to other services.  (#145)
    * WIP remove tapis-jobsmigrate, change schema from public to tapis_jobs.
    
    * Update deployment/jobs-init-db-sh. For info only, not used in production.
    
    * Update deployment/jobs-init-db-sh to move tables from public to tapis_jobs schema.
    
    * Update init-db script with script used in tapis-deploy. This script not used in production.

--- tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/gen/jooq/Tables.java
+++ tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/gen/jooq/Tables.java
@@ -4,6 +4,7 @@
 package edu.utexas.tacc.tapis.jobs.gen.jooq;
 
 
+import edu.utexas.tacc.tapis.jobs.gen.jooq.tables.FlywaySchemaHistory;
 import edu.utexas.tacc.tapis.jobs.gen.jooq.tables.JobBlocked;
 import edu.utexas.tacc.tapis.jobs.gen.jooq.tables.JobEvents;
 import edu.utexas.tacc.tapis.jobs.gen.jooq.tables.JobQueues;
@@ -13,38 +14,43 @@ import edu.utexas.tacc.tapis.jobs.gen.jooq.tables.Jobs;
 
 
 /**
- * Convenience access to all tables in public.
+ * Convenience access to all tables in tapis_jobs.
  */
-@SuppressWarnings({ "all", "unchecked", "rawtypes" })
+@SuppressWarnings({ "all", "unchecked", "rawtypes", "this-escape" })
 public class Tables {
 
     /**
-     * The table <code>public.job_blocked</code>.
+     * The table <code>tapis_jobs.flyway_schema_history</code>.
+     */
+    public static final FlywaySchemaHistory FLYWAY_SCHEMA_HISTORY = FlywaySchemaHistory.FLYWAY_SCHEMA_HISTORY;
+
+    /**
+     * The table <code>tapis_jobs.job_blocked</code>.
      */
     public static final JobBlocked JOB_BLOCKED = JobBlocked.JOB_BLOCKED;
 
     /**
-     * The table <code>public.job_events</code>.
+     * The table <code>tapis_jobs.job_events</code>.
      */
     public static final JobEvents JOB_EVENTS = JobEvents.JOB_EVENTS;
 
     /**
-     * The table <code>public.job_queues</code>.
+     * The table <code>tapis_jobs.job_queues</code>.
      */
     public static final JobQueues JOB_QUEUES = JobQueues.JOB_QUEUES;
 
     /**
-     * The table <code>public.job_recovery</code>.
+     * The table <code>tapis_jobs.job_recovery</code>.
      */
     public static final JobRecovery JOB_RECOVERY = JobRecovery.JOB_RECOVERY;
 
     /**
-     * The table <code>public.job_resubmit</code>.
+     * The table <code>tapis_jobs.job_resubmit</code>.
      */
     public static final JobResubmit JOB_RESUBMIT = JobResubmit.JOB_RESUBMIT;
 
     /**
-     * The table <code>public.jobs</code>.
+     * The table <code>tapis_jobs.jobs</code>.
      */
     public static final Jobs JOBS = Jobs.JOBS;
 }

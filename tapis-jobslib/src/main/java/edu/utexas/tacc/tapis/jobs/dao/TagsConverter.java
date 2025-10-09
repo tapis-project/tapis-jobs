36df42bf5cfd72070b5927485f8be9265751ee10
Wei Zhang <wei.zhang@tacc.utexas.edu>
2025-10-09 11:33:34 -0500

    avoid using complex SQL

--- tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/dao/TagsConverter.java
+++ tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/dao/TagsConverter.java
@@ -5,7 +5,11 @@ import java.sql.Array;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Locale;
+import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 import org.jooq.Converter;
 @SuppressWarnings({ "serial", "rawtypes" })
 public class TagsConverter implements Converter<String[], TreeSet>
@@ -19,6 +23,15 @@ public class TagsConverter implements Converter<String[], TreeSet>
     return (String[]) ts.toArray();
   }
   
+  /**
+   * Convert a TreeSet<String> to a JDBC Array.
+   * Since this is particularly for Tags, we always ensure that it returns a non-null
+   * (but possibly empty) Array.
+   * @param conn
+   * @param tags
+   * @return
+   * @throws SQLException
+   */
   public static Array toJDBCArray(Connection conn, TreeSet<String> tags) throws SQLException {
     Array tagsArray;
     if (tags == null || tags.isEmpty())
@@ -29,9 +42,16 @@ public class TagsConverter implements Converter<String[], TreeSet>
     }
     return tagsArray;
   }
-
+  /**
+   * Convert a JDBC Array to a TreeSet<String>. 
+   * Since this is particularly for Tags, we always ensure that it returns a non-null
+   * (but possibly empty) TreeSet.
+   * @param dbArray
+   * @return
+   * @throws SQLException
+   */
   public static TreeSet<String> fromJDBCArray(Array dbArray) throws SQLException {
-    if (dbArray == null) return null;
+    if (dbArray == null) return new TreeSet<>();
     String[] sa = (String[]) dbArray.getArray();
     return new TreeSet<>(Arrays.asList(sa));
   }
